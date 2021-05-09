fd80b9a10a4f75ae374583106d1f612d9c99add4
oyunseong <gkrlsanjf@naver.com>
2021-05-09 19:05:15 +0900

    Revert "commit"
    This reverts commit 6a2d04d4af3e872d3dab0061358557f92982c135.

--- coding_test/10757.cpp
+++ coding_test/10757.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 string Add(string& s1, string& s2)
 {
-	string result(max(s1.size(), s2.size()),'0');	
+	
+	//_NODISCARD constexpr const _Ty& (max)(const _Ty & _Left, const _Ty & _Right, _Pr _Pred)
+	// left, right, pred를 인자로 받아서 최댓값을 찾습니다.
+	// pred :A binary predicate used to compare the two objects.
+	// 두 개체를 비교하는 데 사용되는 이진 술어입니다.
+	string result(max(s1.size(), s2.size()),'0');	// 더 긴 수를 result로 저장하기
 	bool carry = false;
 
 	for (int i = 0; i < result.size(); i++)
